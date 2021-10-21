8808e6c3707c1a2614056c4155c9e20fddcfce23
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-10-21 12:01:45 +0530

    Complete Todo

--- src/Todo/TodoList.tsx
+++ src/Todo/TodoList.tsx
@@ -4,57 +4,44 @@ import ITodo from '../types/ITodo';
 
 interface Props {
   todoList: ITodo[];
-  filterType: IFilterType;
   completeTodo: (item: ITodo) => void;
   deleteTodo: (id: number) => void;
 }
 
 const TodoList = ({
   todoList,
-  filterType,
   completeTodo,
   deleteTodo,
 }: Props) => {
   console.log('TodoList');
   return (
     <div className="list">
-      {todoList
-        .filter((x: ITodo) => {
-          switch (filterType) {
-            case IFilterType.Completed:
-              return x.isDone;
-            case IFilterType.Pending:
-              return !x.isDone;
-            default:
-              return true;
-          }
-        })
-        .map((item: ITodo) => (
-          <div className="list-item" key={item.id}>
-            <input
-              type="checkbox"
-              name="isDone"
-              id="isDone"
-              checked={item.isDone}
-              onChange={() => completeTodo(item)}
-            />
-            <span
-              style={{
-                textDecoration: item.isDone
-                  ? 'line-through'
-                  : 'none',
-              }}
-            >
-              {item.text}
-            </span>
-            <button
-              type="button"
-              onClick={() => deleteTodo(item.id)}
-            >
-              Delete
-            </button>
-          </div>
-        ))}
+      {todoList.map((item: ITodo) => (
+        <div className="list-item" key={item.id}>
+          <input
+            type="checkbox"
+            name="isDone"
+            id="isDone"
+            checked={item.isDone}
+            onChange={() => completeTodo(item)}
+          />
+          <span
+            style={{
+              textDecoration: item.isDone
+                ? 'line-through'
+                : 'none',
+            }}
+          >
+            {item.text}
+          </span>
+          <button
+            type="button"
+            onClick={() => deleteTodo(item.id)}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
